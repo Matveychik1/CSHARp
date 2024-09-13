fee708aaf9f65218af793d5e6d5e5d3988642686
Matveychik1 <matvijpro25@gmail.com>
2024-09-13 14:25:36 +0300

    daybook

--- frist52/Program.cs
+++ frist52/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("-----------------daybook----------------");
+
+
+Console.Write("ENTER.Rating1=");
+Double rating1 = double.Parse(Console.ReadLine());
+
+
+Console.Write("ENTER.Rating2=");
+Double rating2 = double.Parse(Console.ReadLine());
+
+
+Console.Write("ENTER.Rating3=");
+Double rating3 = double.Parse(Console.ReadLine());
+
+
+Console.Write("ENTER.Rating4=");
+Double rating4 = double.Parse(Console.ReadLine());
+
+
+Console.Write("ENTER.Rating5=");
+Double rating5 = double.Parse(Console.ReadLine());
+
+
+Console.Write("Resulte average score =");
+Console.Write((rating1 + rating2 + rating3 + rating4 + rating5) / 5);
