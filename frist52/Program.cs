2b172b6727c0146c8cb2f6ab73b9d9af99ca85f6
mka <mka@A34PC9>
2024-09-16 14:25:52 +0300

    mars

--- frist52/Program.cs
+++ frist52/Program.cs
@@ -1,10 +1,20 @@
-﻿Console.WriteLine("-----------------Weather degrees----------------");
+﻿//Console.WriteLine("-----------------Weather degrees----------------");
 
 
-Console.Write("ENTER.degrees=");
-Double degrees = double.Parse(Console.ReadLine());
+//Console.Write("ENTER.degrees=");
+//Double degrees = double.Parse(Console.ReadLine());
+//double f = degrees * 1.8 + 32;
+
+//Console.Write("Resulte fahrenheit =");
+//Console.WriteLine(Math.Round(f));
 
 
-Console.Write("Resulte fahrenheit =");
-Console.Write(degrees * 1.8 + 32);
-object value = Math.Round(int degrees);
+Console.WriteLine("-----------------mars and earz----------------");
+const int revolution = 686;
+Console.Write("ENTER.oborot=");
+int oborot = int.Parse(Console.ReadLine());
+Console.Write("dayoborot =");
+Console.Write(revolution * oborot );
+
+int doborot = int.Parse(Console.ReadLine());
+
